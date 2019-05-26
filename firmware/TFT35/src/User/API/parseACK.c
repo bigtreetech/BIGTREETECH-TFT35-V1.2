3dcbd68db5cc877ec616a9bf7632eb98a806e024
dianlight <lucio.tarantino@gmail.com>
2019-05-26 14:38:06 +0200

    code cleanup

--- firmware/TFT35/src/User/API/parseACK.c
+++ firmware/TFT35/src/User/API/parseACK.c
@@ -2,6 +2,7 @@
 
 static const char errormagic[]  = "Error:";
 static const char echomagic[]   = "echo:";
+static const char busymagic[]   = "busy:";
 
 char ack_rev_buf[ACK_MAX_SIZE];
 static u16 ack_index=0;
@@ -39,9 +40,6 @@ static float ack_value()
   return (strtod(&ack_rev_buf[ack_index], NULL));
 }
 
-extern SCROLL   titleScroll;
-extern GUI_RECT titleRect;
-
 void parseACK(void)
 {
   if(infoHost.rx_ok != true) return;      // 未收到应答数据
@@ -73,7 +71,7 @@ void parseACK(void)
     {
       heatSetCurrentTemp(BED,ack_value()+0.5);
     }
-    else if(infoHost.connected && ack_seen(echomagic) && ack_seen("busy:") && ack_seen("processing") && infoMenu.menu[infoMenu.cur] != menuPopup)
+    else if(infoHost.connected && ack_seen(echomagic) && ack_seen(busymagic) && ack_seen("processing") && infoMenu.menu[infoMenu.cur] != menuPopup)
     {
       busyIndicator(STATUS_BUSY);
     }
@@ -82,6 +80,11 @@ void parseACK(void)
       if(ack_seen(errormagic))
       {
         popupSetContext((u8* )errormagic, (u8 *)ack_rev_buf + ack_index, textSelect(LABEL_CONFIRM), NULL);
+        infoMenu.menu[++infoMenu.cur] = menuPopup;
+      }
+      else if(ack_seen(busymagic))
+      {
+        popupSetContext((u8* )busymagic, (u8 *)ack_rev_buf + ack_index, textSelect(LABEL_CONFIRM), NULL);
         infoMenu.menu[++infoMenu.cur] = menuPopup;
       }
       else if(infoHost.connected && ack_seen(echomagic))
